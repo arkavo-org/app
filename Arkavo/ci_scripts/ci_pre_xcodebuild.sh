8aa669a11c6da3be730553e18e75a4ccadd1c649
Paul Flynn <paul@arkavo.com>
2024-04-20 11:42:48 -0400

    bzip

--- Arkavo/ci_scripts/ci_pre_xcodebuild.sh
+++ Arkavo/ci_scripts/ci_pre_xcodebuild.sh
@@ -6,9 +6,13 @@ set -e
 
 # install conan
 brew install --quiet conan
+conan version
 
 # conan profile
 conan profile detect
+
+# install bzip2 (it fails in Xcode Cloud, so try it first)
+conan install bzip2_conanfile.txt --build=missing -g=XcodeDeps
 
 # install dependencies
 conan install conanfile.txt --build=missing -g=XcodeDeps
