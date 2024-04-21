a8c5c8370431b417be20577b56093130c0d70862
Paul Flynn <paul@arkavo.com>
2024-04-20 20:46:28 -0400

    libxml2

--- Arkavo/ci_scripts/ci_pre_xcodebuild.sh
+++ Arkavo/ci_scripts/ci_pre_xcodebuild.sh
@@ -14,8 +14,10 @@ conan version
 conan profile detect --force
 
 # install bzip2 (it fails in Xcode Cloud, so try it first)
-curl -I https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz
 conan install bzip2_conanfile.txt --settings build_type=Release --build=missing --output-folder=../../opentdf-client
+
+# install libxml2 (it fails in Xcode Cloud, so try it second)
+conan install libxml2_conanfile.txt --settings build_type=Release --build=missing --output-folder=../../opentdf-client
 
 # install dependencies
 conan install conanfile.txt --settings build_type=Release --build=missing --output-folder=../../opentdf-client
