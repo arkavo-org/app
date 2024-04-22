c133b407cd3b632ce03fdd187984a14d0885daf3
Paul Flynn <paul@arkavo.com>
2024-04-21 21:13:05 -0400

    tdf encrypt (#4)

--- Arkavo/Arkavo/Services/OpenTDFWrapper.mm
+++ Arkavo/Arkavo/Services/OpenTDFWrapper.mm
@@ -20,7 +20,7 @@ const std::string cert_authority = "CertAuthority";
     oidcCreds.setClientCredentialsClientSecret(client_id, client_secret, organization_name, oidc_endpoint);
     // Test KAS endpoint - curl https://arkavo.net/kas/v2/kas_public_key
     auto client = virtru::TDFClient(oidcCreds, kas_domain);
-    client.enableConsoleLogging();
+//    client.enableConsoleLogging();
     // create TDF
     virtru::TDFStorageType tdfStorageType;
     tdfStorageType.setTDFStorageStringType("Hello world");
@@ -34,4 +34,40 @@ const std::string cert_authority = "CertAuthority";
         NSLog(@"%s", word.c_str());
     }
 }
+
+- (NSData *)encrypt:(NSString *)input {
+    NSLog(@"%s", "encrypt");
+    virtru::OIDCCredentials oidcCreds;
+    oidcCreds.setClientCredentialsClientSecret(client_id, client_secret, organization_name, oidc_endpoint);
+    // Test KAS endpoint - curl https://arkavo.net/kas/v2/kas_public_key
+    auto client = virtru::TDFClient(oidcCreds, kas_domain);
+//    client.enableConsoleLogging();
+    // create TDF
+    virtru::TDFStorageType tdfStorageType;
+    std::string cppString = std::string([input UTF8String]);
+    tdfStorageType.setTDFStorageStringType(cppString);
+    auto mytdf = client.encryptData(tdfStorageType);
+    return [[NSData alloc] initWithBytes:mytdf.data() length:mytdf.size()];
+}
+
+- (NSString *)decrypt:(NSData *)input {
+    NSLog(@"%s", "decrypt");
+    virtru::OIDCCredentials oidcCreds;
+    oidcCreds.setClientCredentialsClientSecret(client_id, client_secret, organization_name, oidc_endpoint);
+    // Test KAS endpoint - curl https://arkavo.net/kas/v2/kas_public_key
+    auto client = virtru::TDFClient(oidcCreds, kas_domain);
+//    client.enableConsoleLogging();
+    // decrypt TDF
+    virtru::TDFStorageType tdfStorageType;
+    NSUInteger length = [input length];
+    std::vector<virtru::VBYTE> buffer(length);
+    memcpy(&buffer[0], [input bytes], length);
+    tdfStorageType.setTDFStorageBufferType(buffer);
+    auto vectorData = client.decryptData(tdfStorageType);
+    // Convert std::vector<VBYTE> to NSData
+    NSData *data = [NSData dataWithBytes:vectorData.data() length:vectorData.size()];
+    // Convert NSData to NSString
+    return [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+}
+
 @end
--- Arkavo/ci_scripts/ci_pre_xcodebuild.sh
+++ Arkavo/ci_scripts/ci_pre_xcodebuild.sh
@@ -19,8 +19,19 @@ conan profile detect --force
 # install libxml2 (it fails in Xcode Cloud, so try it second)
 #conan install libxml2_conanfile.txt --settings build_type=Release --build=missing --output-folder=../../opentdf-client
 
-# install dependencies Release and Debug for macOS and iOS
+# install dependencies Release and Debug for macOS and iOS (uncomment for all platforms)
 #conan install conanfile.txt --profile conanprofile_macos.txt --settings build_type=Release --build=missing --output-folder=../../opentdf-client
 #conan install conanfile.txt --profile conanprofile_macos.txt --settings build_type=Debug --build=missing --output-folder=../../opentdf-client
 conan install conanfile.txt --profile conanprofile_ios.txt --settings build_type=Release --build=missing --output-folder=../../opentdf-client
 conan install conanfile.txt --profile conanprofile_ios.txt --settings build_type=Debug --build=missing --output-folder=../../opentdf-client
+
+# fix for skipped dependencies (TODO figure why and fix)
+input_file_path="../../opentdf-client/build/Debug/generators/conan_opentdf_client_libopentdf.xcconfig"
+# Create a temporary file that will eventually replace the original file
+temp_file=$(mktemp)
+# Remove lines containing the specified strings using sed
+sed -e '/#include "conan_ms_gsl.xcconfig"/d' -e '/#include "conan_nlohmann_json.xcconfig"/d' -e '/#include "conan_magic_enum.xcconfig"/d' "$input_file_path" > "$temp_file"
+# Replace the original file with the modified temporary file
+mv "$temp_file" "$input_file_path"
+# Give the user read and write permission to the modified original file
+chmod 644 "$input_file_path"
