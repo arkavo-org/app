ed9e506629f2a847e4dee2c54c2d78bb97a72c6c
Paul Flynn <paul@arkavo.com>
2024-07-11 23:05:26 -0400

    Remove outdated comments from signIn function
    The unused comments in the signIn() function of the AuthenticationManager.swift file were removed. These comments were providing outdated instructions which are no longer applicable or necessary. This clean-up improves code readability and removes confusion.

--- Arkavo/Arkavo/AuthenticationManager.swift
+++ Arkavo/Arkavo/AuthenticationManager.swift
@@ -72,9 +72,7 @@ class AuthenticationManager: NSObject, ASAuthorizationControllerDelegate, ASAuth
     }
 
     func signIn() {
-        // You can implement the sign-in functionality similar to the signUp() method
-        // But keep in mind you'll most likely have to use createCredentialAssertionRequest instead of createCredentialRegistrationRequest
-        // FIXME send session token to opentdf-client
+        // Handle successful signIn
     }
 
     func authorizationController(controller _: ASAuthorizationController, didCompleteWithAuthorization _: ASAuthorization) {
