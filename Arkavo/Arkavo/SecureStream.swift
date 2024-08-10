4cf1c6270639f94912028af381fa227c646f45b0
Paul Flynn <paul@arkavo.com>
2024-08-10 17:15:51 -0400

    Add Codable conformance to SecureStream
    Implemented Codable conformance for SecureStream class to support encoding and decoding. Added necessary CodingKeys enumeration and init(from:) initializer, as well as an encode(to:) method. Introduced serialize and deserialize methods for binary property list serialization and deserialization.

--- Arkavo/Arkavo/SecureStream.swift
+++ Arkavo/Arkavo/SecureStream.swift
@@ -3,22 +3,58 @@ import Foundation
 import SwiftData
 
 @Model
-final class SecureStream: Identifiable {
+final class SecureStream: Identifiable, Codable {
     @Attribute(.unique) var id: UUID
     var name: String
     var createdAt: Date
-    var updatedAt: Date
     var tags: [String]
     var ownerID: UUID
     @Relationship var profile: Profile?
+    private static let decoder = PropertyListDecoder()
+    private static let encoder: PropertyListEncoder = {
+        let encoder = PropertyListEncoder()
+        encoder.outputFormat = .binary
+        return encoder
+    }()
+
+    enum CodingKeys: String, CodingKey {
+        case id, name, createdAt, updatedAt, tags, ownerID, profile
+    }
 
     init(id: UUID = UUID(), name: String, ownerID: UUID) {
         self.id = id
         self.name = name
         self.createdAt = Date()
-        self.updatedAt = Date()
         self.tags = []
         self.ownerID = ownerID
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(UUID.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        createdAt = try container.decode(Date.self, forKey: .createdAt)
+        tags = try container.decode([String].self, forKey: .tags)
+        ownerID = try container.decode(UUID.self, forKey: .ownerID)
+        profile = try container.decodeIfPresent(Profile.self, forKey: .profile)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(createdAt, forKey: .createdAt)
+        try container.encode(tags, forKey: .tags)
+        try container.encode(ownerID, forKey: .ownerID)
+        try container.encodeIfPresent(profile, forKey: .profile)
+    }
+    
+    func serialize() throws -> Data {
+        try SecureStream.encoder.encode(self)
+    }
+
+    static func deserialize(from data: Data) throws -> SecureStream {
+        try decoder.decode(SecureStream.self, from: data)
     }
 }
 
