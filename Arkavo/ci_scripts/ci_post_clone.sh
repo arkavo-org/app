ae7c6d5ba1acef793c35be8639e7d8aed2a26d01
Paul Flynn <paul@arkavo.com>
2024-11-30 09:18:12 -0500

    Update Secrets.swift path in post-clone script
    Adjusted the file path for Secrets.swift to use the CI_WORKSPACE environment variable. Added debug echo statements to verify the paths and ensure the script behaves as expected.

--- Arkavo/ci_scripts/ci_post_clone.sh
+++ Arkavo/ci_scripts/ci_post_clone.sh
@@ -2,10 +2,12 @@
 
 # Ensure the script stops on error
 set -e
-
+echo $CI_WORKSPACE
+echo $CI_PROJECT_DIR
 # Define the Secrets.swift file path relative to the Git repository root
-SECRETS_FILE="${CI_PROJECT_DIR}/ArkavoCreator/Secrets.swift"
-
+SECRETS_FILE="${CI_WORKSPACE}/Arkavo/ArkavoCreator/Secrets.swift"
+echo $SECRETS_FILE
+ls -l
 # Ensure required environment variables are set
 if [[ -z "${PATREON_CLIENT_ID}" || -z "${PATREON_CLIENT_SECRET}" ]]; then
   echo "Error: Required environment variables PATREON_CLIENT_ID or PATREON_CLIENT_SECRET are not set."
